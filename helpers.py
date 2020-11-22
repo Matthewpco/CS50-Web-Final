a78c7808cfcfb1c7623f2cfb19c73f09f235849d
Matthew Payne <71348063+Matthewpco@users.noreply.github.com>
2020-11-22 11:46:40 -0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import os
 import requests
 import urllib.parse
 
+from webconfig import API_KEY
 from flask import redirect, render_template, request, session
 from functools import wraps
 
@@ -40,7 +41,7 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
+        api_key = API_KEY
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
